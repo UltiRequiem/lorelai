a7eef5c78d42546e8e4f95182fd4d0f30e3781c9
Antoineio <antoine@inovalyte.fr>
2025-03-13 21:36:04 +0100

    added comments mentioning RFC 1035 as discussed in #8

--- pkg/tlds_test.go
+++ pkg/tlds_test.go
@@ -5,6 +5,8 @@ import (
 )
 
 func TestTLDSLength(t *testing.T) {
+	// RFC 1035 recommends limiting labels to 63 characters.
+	// See: https://www.rfc-editor.org/rfc/rfc1035#section-2.3.4
 	for _, tld := range TLDS {
 		if len(tld) < 2 || len(tld) > 63 {
 			t.Errorf("Invalid TLD '%s' (length: %d). Must be between 2 and 63 characters", tld, len(tld))
